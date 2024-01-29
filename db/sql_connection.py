37144590d7823d5b245f918c07c736c3458aded5
Gabriel Moscarde <gabrielmoscarde@outlook.com>
2024-01-28 23:48:07 -0300

    add script for populate db

--- db/sql_connection.py
+++ db/sql_connection.py
@@ -19,7 +19,8 @@ def start_database():
         birth TEXT,
         email TEXT,
         address TEXT,
-        instagram TEXT
+        instagram TEXT,
+        observations TEXT
     )
 """
     )
@@ -30,11 +31,13 @@ def start_database():
     CREATE TABLE IF NOT EXISTS tattoos (
         id INTEGER PRIMARY KEY,
         customer_id INTEGER NOT NULL,
-        tattoo TEXT NOT NULL,
+        tattoo_name TEXT NOT NULL,
         description TEXT,
         price INTEGER,
+        comission INTEGER,
         payment TEXT,
         date TEXT,
+        time TEXT,
         status TEXT,
         FOREIGN KEY (customer_id) REFERENCES customers (id) ON DELETE CASCADE
     )"""
@@ -50,7 +53,147 @@ def connect_database():
     return conn, c
 
 
-if __name__ == "__main__":
+def populate_database(table, data):
+    conn, c = connect_database()
 
+    if table == "customers":
+        query = f"""
+            INSERT INTO customers (name, phone, birth, email, address, instagram, observations)
+            VALUES
+                ('{data["name"]}', {data["phone"]}, '{data["birth"]}', '{data["email"]}', '{data["address"]}', '{data["instagram"]}', '{data["observations"]}')
+            
+    """
+
+        c.execute(query)
+        conn.commit()
+        conn.close()
+
+    elif table == "tattoos":
+        query = f"""
+            INSERT INTO tattoos (customer_id, tattoo_name, description, price, comission, payment, date, time, status)
+            VALUES
+                ({data["customer_id"]}, '{data["tattoo_name"]}', '{data["description"]}', {data["price"]}, {data["comission"]}, '{data["payment"]}', '{data["date"]}', '{data["time"]}', '{data["status"]}')
+                """
+
+        c.execute(query)
+        conn.commit()
+        conn.close()
+
+
+if __name__ == "__main__":
     start_database()
-    print('Base criada')
+    print("Base criada")
+
+    data_customers = [
+        {
+            "name": "Joao Silva",
+            "phone": 123456789,
+            "birth": "1990-05-15",
+            "email": "joao@example.com",
+            "address": "Rua A, 123",
+            "instagram": "@joaosilva",
+            "observations": "obs",
+        },
+        {
+            "name": "Maria Oliveira",
+            "phone": 123456789,
+            "birth": "1990-05-15",
+            "email": "maria@example.com",
+            "address": "Rua B, 456",
+            "instagram": "@mariaoliveira",
+            "observations": "obs",
+        },
+        {
+            "name": "Carlos Pereira",
+            "phone": 123456789,
+            "birth": "1985-09-20",
+            "email": "carlos@example.com",
+            "address": "Rua C, 789",
+            "instagram": "@carlosp",
+            "observations": "obs",
+        },
+        {
+            "name": "Joaquim Costa",
+            "phone": 123456789,
+            "birth": "1985-09-20",
+            "email": "joaquim@example.com",
+            "address": "Rua D, 987",
+            "instagram": "@joaquimcosta",
+            "observations": "obs",
+        },
+        {
+            "name": "Ana Souza",
+            "phone": 123456789,
+            "birth": "1985-09-20",
+            "email": "ana@example.com",
+            "address": "Rua E, 654",
+            "instagram": "@anasouza",
+            "observations": "obs",
+        },
+    ]
+
+    data_tattoos = [
+        {
+            "customer_id": 1,
+            "tattoo_name": "Tattoo 1",
+            "description": "Description 1",
+            "price": 100,
+            "comission": 40,
+            "payment": "Cash",
+            "date": "2022-01-01",
+            "time": "10:00",
+            "status": "Done",
+        },
+        {
+            "customer_id": 2,
+            "tattoo_name": "Tattoo 2",
+            "description": "Description 2",
+            "price": 200,
+            "comission": 40,
+            "payment": "Credit Card",
+            "date": "2022-02-02",
+            "time": "11:00",
+            "status": "Scheduled",
+        },
+        {
+            "customer_id": 3,
+            "tattoo_name": "Tattoo 3",
+            "description": "Description 3",
+            "price": 300,
+            "comission": 60,
+            "payment": "Cash",
+            "date": "2022-03-03",
+            "time": "12:00",
+            "status": "Scheduled",
+        },
+        {
+            "customer_id": 4,
+            "tattoo_name": "Tattoo 4",
+            "description": "Description 4",
+            "price": 400,
+            "comission": 60,
+            "payment": "Credit Card",
+            "date": "2022-04-04",
+            "time": "13:00",
+            "status": "Scheduled",
+        },
+        {
+            "customer_id": 4,
+            "tattoo_name": "Tattoo 5",
+            "description": "Description 5",
+            "price": 500,
+            "comission": 100,
+            "payment": "Cash",
+            "date": "2022-05-05",
+            "time": "14:00",
+            "status": "Done",
+        },
+    ]
+
+    for customer in data_customers:
+        populate_database("customers", customer)
+
+    for tattoo in data_tattoos:
+        populate_database("tattoos", tattoo)
+
+    print("Base preenchida")
